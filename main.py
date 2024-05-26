22dc8bc2ba9974348166b8c950f95d64164677b7
facu-corvalan <adrian.facundo2001@gmail.com>
2024-05-25 22:18:02 -0300

    Finalizando los endpoint

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = FastAPI()
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-@app.get("/Developer", tags=["Desarrollador"])
+@app.get("/developer", tags=["Desarrollador"])
 
-async def developer(developer : str = Query(default='Kotoshiro')):
+async def developer(developer : str = Query(default='Bohemia Interactive')):
     """
     End point 1
     - def developer( desarrollador : str ): Cantidad de items y porcentaje de contenido Free
@@ -26,15 +26,15 @@ async def developer(developer : str = Query(default='Kotoshiro')):
     # Filtramos por el desarrollador ingresado
     games = games[games['developer'] == developer]
 
-    # Agrupamos por año y contamos la cantidad de items
-    games['Año'] = games['release_date'].dt.year
-    games_year = games.groupby('Año').size().reset_index(name='Cantidad de Items')
+    # Agrupamos por release_year y contamos la cantidad de items
+    games['release_year'] = games['release_date'].dt.year
+    games_year = games.groupby('release_year').size().reset_index(name='Cantidad de Items')
 
-    # Agrupamos por año y sumamos la cantidad de items free
-    games_free = games.groupby('Año')['free'].sum().reset_index(name='free')
+    # Agrupamos por release_year y sumamos la cantidad de items free
+    games_free = games.groupby('release_year')['free'].sum().reset_index(name='free')
 
-    # Hacemos un merge de ambos dataframes mediante la columna Año
-    games = pd.merge(games_year, games_free, on='Año')
+    # Hacemos un merge de ambos dataframes mediante la columna release_year
+    games = pd.merge(games_year, games_free, on='release_year')
 
     # Calculamos el porcentaje de contenido free
     games['Contenido Free'] = round(games['free'] / games['Cantidad de Items'] * 100, 2)
@@ -47,53 +47,67 @@ async def developer(developer : str = Query(default='Kotoshiro')):
     
     return games
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+@app.get('/userdata', tags=['Informacion del Jugador'])
 
-@app.get('/UserForGenre', tags=['Jugador por Género'])
+def userdata( User_id : str ):
+    """
+    def userdata( User_id : str ): Debe devolver cantidad de dinero gastado por el usuario, el porcentaje de recomendación en base a reviews.recommend y cantidad de items.
+    Ejemplo de retorno: {"Usuario X" : us213ndjss09sdf, "Dinero gastado": 200 USD, "% de recomendación": 20%, "cantidad de items": 5}
+    """
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.get('/User_For_Genre', tags=['Jugador por Género'])
 
 def UserForGenre(genre: str = Query(default='Action')):
     """
-    3) enpoint:
+    Endpoint para obtener el usuario con más horas jugadas y la acumulación de horas jugadas por año de lanzamiento para un género dado.
 
-    - def UserForGenre( genero : str ): Debe devolver el usuario que acumula más horas jugadas para el género dado 
-      y una lista de la acumulación de horas jugadas por año de lanzamiento.
-
-    - Ejemplo de retorno: 
-    {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}
-    , {Año: 2011, Horas: 23}]}
+    Ejemplo de retorno:
+    {
+        "Usuario con más horas jugadas para Género X": "us213ndjss09sdf",
+        "Horas jugadas": [
+            {"Año": 2013, "Horas jugadas": 203},
+            {"Año": 2012, "Horas jugadas": 100},
+            {"Año": 2011, "Horas jugadas": 23}
+        ]
+    }
     """
     games = pd.read_parquet('Dataset/user_genre_games.parquet')
     items = pd.read_parquet('Dataset/user_genre_items.parquet')
-    
-    # Filtrando los juegos por el género dado
+    # Filtrar los juegos por el género dado
     games_genre = games[games['genres'].str.contains(genre, case=False, na=False)]
     
-    # Combinando los DataFrames games e items
+    # Combinar los DataFrames de juegos e items
     filter_games = games_genre.merge(items, on='id', how='inner')
     
-    # Añadiendo la columna del año de lanzamiento
+    # Añadir la columna del año de lanzamiento
     filter_games['release_year'] = filter_games['release_date'].dt.year
     
-    # Agrupando por user_id y release_year para sumar el tiempo de juego
-    user_year = filter_games.groupby(['id', 'release_year']).agg({'playtime_forever': 'sum'}).reset_index()
+    # Agrupar por user_id y release_year para sumar el tiempo de juego
+    user_year = filter_games.groupby(['user_id', 'release_year'])['playtime_forever'].sum().reset_index()
     
-    # Encontrando el usuario con más horas jugadas
-    total_playtime_per_user = user_year.groupby('user_id')['playtime_forever'].sum()
-    max_user = total_playtime_per_user.idxmax()
+    # Encontrar el usuario con más horas jugadas
+    max_user = user_year.loc[user_year['playtime_forever'].idxmax(), 'user_id']
     
-    # Filtrando las horas jugadas por año para el usuario con más horas
+    # Filtrar las horas jugadas por año para el usuario con más horas
     user_playtime_by_year = user_year[user_year['user_id'] == max_user][['release_year', 'playtime_forever']]
     
-    # Renombrando las columnas para cumplir con el formato requerido
+    # Renombrar las columnas para cumplir con el formato requerido
     user_playtime_by_year.rename(columns={'release_year': 'Año', 'playtime_forever': 'Horas jugadas'}, inplace=True)
     
-    # Convirtiendo el resultado a un formato de lista de diccionarios
+    # Convertir el resultado a un formato de lista de diccionarios
     year_playtime = user_playtime_by_year.to_dict('records')
     
-    # Devolviendo el resultado en el formato especificado
-    return {"Usuario con más horas jugadas para Género " + genre: max_user, "Horas jugadas": year_playtime}
+    return {
+        "Usuario con más horas jugadas para Género " + genre: max_user,
+        "Horas jugadas": year_playtime
+    }
+
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @app.get('/best_developer_year', tags=['Mejor Desarrollador'])
+
 def best_developer_year( año : int= Query(default='2000' )):
     """
     4) enpoint:
@@ -125,3 +139,36 @@ def best_developer_year( año : int= Query(default='2000' )):
         {"Puesto 2": top_developers[1]},
         {"Puesto 3": top_developers[2]}]
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.get('/developer_reviews_analysis', tags=['Reseña de Desarrolladores'])
+
+def developer_reviews_analysis(desarrolladora: str = Query(default='Bohemia Interactive')):
+    """
+    Según el desarrollador, se devuelve un diccionario con el nombre del desarrollador como llave
+    y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados
+    con un análisis de sentimiento como valor positivo o negativo.
+
+    Ejemplo de retorno: {'Valve': {'Negativas': 182, 'Positivas': 278}}
+    """
+    games = pd.read_parquet('Dataset/developer_review_games.parquet')  # Cargar datos de juegos
+    reviews = pd.read_parquet('Dataset/developer_review_reviews.parquet')  # Cargar datos de reseñas
+
+    # Filtrar los juegos por el desarrollador especificado
+    developer_games = games[games['developer'] == desarrolladora]
+    
+    # Combinar datos de juegos y reseñas basado en el id del juego
+    merged_data = developer_games.merge(reviews, left_on='id', right_on='item_id', how='inner')
+
+    # Verificar si no hay datos disponibles para el desarrollador
+    if merged_data.empty:
+        return {desarrolladora: {'Negativas': 0, 'Positivas': 0}}
+
+    # Calcular el total de reseñas negativas y positivas
+    total_negativas = merged_data['negativo'].sum()
+    total_positivas = merged_data['positivo'].sum()
+
+    # Retornar el resultado en el formato esperado
+    return {desarrolladora: {'Negativas': int(total_negativas), 'Positivas': int(total_positivas)}}
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
